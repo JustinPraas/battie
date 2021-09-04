920d92bb7c4e0e358c8f32611e3386e725b9a9b7
unknown <jw.praas2@gmail.com>
2021-09-04 10:34:29 +0200

    Fix user roulette

--- src/modules/utilities/user_roulette.ts
+++ src/modules/utilities/user_roulette.ts
@@ -1,56 +1,59 @@
-// import { Collection, GuildMember, Message, Snowflake, User } from "discord.js";
-// import { Command } from "../../models/Command";
+import { CommandInteraction, User } from "discord.js";
+import { Command } from "../../models/Command";
 
-// const COMMAND = "roulette"
+export const userRoulette: Command = {
+    command: {
+        name: 'roulette',
+        description: 'Kiest een willekeurige gebruiker uit de aangegeven lijst van users',
+        options: [
+            {
+                name: 'users',
+                type: 'STRING' as const,
+                description: 'De users, gesplits door een komma',
+                required: true,
+            }],
+    },
+    async execute(interaction, _1, _2) {
+        const usersArg = interaction.options.get("users")?.value as string
+        const users = usersArg.split("<@!").map(s => "<@!" + s).slice(1)
+        const names = usersArg.split(" ")
+        if (users.length > 0) {
+            const chosenUser = users[Math.floor(Math.random() * users.length)];
+            sendMessageStrings(interaction, chosenUser);
+        } else if (names.length > 0) {
+            const chosenName = names[Math.floor(Math.random() * names.length)];
+            sendMessageStrings(interaction, chosenName);
+        } else {
+            await interaction.reply("Geef eens een aantal opties mee...")
+            return;
+        }
+    },
+};
 
-// export const userRoulette: Command = {
-//     name: COMMAND,
-//     format: `\`${COMMAND} [@user1] [@user2] ...\` of \`${COMMAND} [naam1] [naam2]...\``,
-//     description: "Kiest een willekeurige gebruiker uit de aangegeven lijst van gebruikers.",
-//     execute(message, args) {
-        
-//         const users: Collection<Snowflake, GuildMember> | null = message.mentions.members;
+async function sendMessageStrings(interaction: CommandInteraction, chosenPerson: string) {
+    await interaction.reply(getMessageVariant()(chosenPerson));
+}
 
-//         if (users) {
-//             if (users.size == 0) {
-//                 const names = args
-//                 const chosenName = names[Math.floor(Math.random() * names.length)];
-//                 sendMessageStrings(message, chosenName);
-//             } else {
-//                 const usersArray = users.map(gm => gm.user);
-//                 const chosenUser = usersArray[Math.floor(Math.random() * usersArray.length)];
-//                 sendMessageMentions(message, chosenUser);
-//             }
-//         } else {
-//             message.channel.send("Er is wat fout gegaan en ik heb geen idee wat. Probeer het opnieuw questionmark?")
-//         }
-//     },
-// };
-
-// function sendMessageStrings(message: Message, chosenPerson: string) {
-//     message.channel.send(getMessageVariant()(chosenPerson));
+// async function sendMessageMentions(interaction: CommandInteraction, chosenUser: string) {
+//     await interaction.reply(getMessageVariant()(getMention(chosenUser)));
 // }
 
-// function sendMessageMentions(message: Message, chosenUser: User) {
-//     message.channel.send(getMessageVariant()(getMention(chosenUser)));
-// }
+const getMessageVariant = () => MESSAGE_VARIANTS[Math.floor(Math.random() * MESSAGE_VARIANTS.length)];
 
-// const getMessageVariant = () => MESSAGE_VARIANTS[Math.floor(Math.random() * MESSAGE_VARIANTS.length)];
+const messageVariant1 = (chosenName: string): string => {
+    return `Jaja, ${chosenName} is de gelukkige!`
+}
 
-// const messageVariant1 = (chosenName: string): string => {
-//     return `Jaja, ${chosenName} is de gelukkige!`
-// }
+const messageVariant2 = (chosenName: string): string => {
+    return `${chosenName} mag de afwas doen.`
+}
 
-// const messageVariant2 = (chosenName: string): string => {
-//     return `${chosenName} mag de afwas doen.`
-// }
+const messageVariant3 = (chosenName: string): string => {
+    return `${chosenName} heeft gewonnen. Hoppa`
+}
 
-// const messageVariant3 = (chosenName: string): string => {
-//     return `${chosenName} heeft gewonnen. Hoppa`
-// }
+const getMention = (author: User) => {
+    return `<@${author}>`
+}
 
-// const getMention = (author: User) => {
-//     return `<@${author}>`
-// }
-
-// const MESSAGE_VARIANTS = [messageVariant1, messageVariant2, messageVariant3]
+const MESSAGE_VARIANTS = [messageVariant1, messageVariant2, messageVariant3]
--- src/modules/utilities/utilities-module.ts
+++ src/modules/utilities/utilities-module.ts
@@ -1,5 +1,6 @@
 import { help } from "./help";
 import { poll } from "./poll";
 import { roll } from "./roll";
+import { userRoulette } from "./user_roulette";
 
-export const utilityCommands = [ help, poll, roll ]
+export const utilityCommands = [ help, poll, roll, userRoulette ]
